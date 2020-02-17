0657a4670f0fcc64bc665cf08768a6d6004f7bc0
David Courtet <david.courtet@enovating.com>
2020-02-17 09:28:08 +0100

    Family field with links to Espacenet.

--- src/enovating.POT.MSW/Template/Writers/FamilyWriter.cs
+++ src/enovating.POT.MSW/Template/Writers/FamilyWriter.cs
@@ -30,7 +30,7 @@ namespace enovating.POT.MSW.Template.Writers
         /// <inheritdoc />
         public bool Can(string code, Patent value)
         {
-            return code == "Family" && value.Family != null;
+            return value.Family != null && (code == "Family" || code == "FamilyLK");
         }
 
         /// <summary>
@@ -48,11 +48,23 @@ namespace enovating.POT.MSW.Template.Writers
         /// </summary>
         /// <param name="target">The document target range.</param>
         /// <param name="value">The value.</param>
-        private void Insert(Range target, PatentLinks value)
+        /// <param name="address">The URL address.</param>
+        private void Insert(Range target, string value, string address)
         {
             var previous = target.Font.Size;
-            var element = target.Hyperlinks.Add(target, value.FullText, TextToDisplay: "PDF");
+            var element = target.Hyperlinks.Add(target, address, TextToDisplay: value);
             element.Range.Font.Size = previous;
+        }
+
+        /// <summary>
+        ///     Inserts the value into the document target range.
+        /// </summary>
+        /// <param name="target">The document target range.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="address">The URL address.</param>
+        private void Insert(Range target, PatentNumber value, string address)
+        {
+            Insert(target, value.ToString(), address);
         }
 
         /// <summary>
@@ -82,11 +94,21 @@ namespace enovating.POT.MSW.Template.Writers
 
             for (var index = 0; index < table.Rows.Count; index++)
             {
-                Insert(table.Cell(index + 1, 1).Range, value.Family[index].PublicationNumber);
-                Insert(table.Cell(index + 1, 2).Range, value.Family[index].PublicationDate);
-                Insert(table.Cell(index + 1, 3).Range, value.Family[index].ApplicationNumber);
-                Insert(table.Cell(index + 1, 4).Range, value.Family[index].ApplicationDate);
-                Insert(table.Cell(index + 1, 5).Range, value.Family[index].Links);
+                var current = value.Family[index];
+
+                if (code == "FamilyLK")
+                {
+                    Insert(table.Cell(index + 1, 1).Range, current.PublicationNumber, current.Links.Espacenet);
+                }
+                else
+                {
+                    Insert(table.Cell(index + 1, 1).Range, current.PublicationNumber);
+                }
+
+                Insert(table.Cell(index + 1, 2).Range, current.PublicationDate);
+                Insert(table.Cell(index + 1, 3).Range, current.ApplicationNumber);
+                Insert(table.Cell(index + 1, 4).Range, current.ApplicationDate);
+                Insert(table.Cell(index + 1, 5).Range, "PDF", current.Links.FullText);
             }
         }
     }
